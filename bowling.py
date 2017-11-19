705de993e0d7b9af8dbe2ebb1dc3d5d0b132516e
Adrienn Dánics <adrienn.d91@gmail.com>
2017-11-19 15:09:22 +0100

    get_value simplified by listing numbers, PEP8

--- bowling.py
+++ bowling.py
@@ -9,7 +9,7 @@ def score(game):
             result += get_value(game[i])
         # if not in_first_half:
             # frame += 1
-        if frame < 10  and get_value(game[i]) == 10:
+        if frame < 10 and get_value(game[i]) == 10:
             if game[i] == '/':
                 result += get_value(game[i+1])
             elif game[i] == 'X' or game[i] == 'x':
@@ -21,7 +21,7 @@ def score(game):
         last = get_value(game[i])
         if not in_first_half:
             frame += 1
-        if in_first_half == True:
+        if in_first_half is True:
             in_first_half = False
         else:
             in_first_half = True
@@ -30,10 +30,9 @@ def score(game):
             frame += 1
     return result
 
+
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+    if char in str([i for i in range(1, 10)]):
         return int(char)
     elif char == 'X' or char == 'x':
         return 10
