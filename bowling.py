08eec0e3faaee54a96a432763b44dddcadf7bc94
Adrienn Dánics <adrienn.d91@gmail.com>
2017-11-19 18:19:44 +0100

    in_first_half variable removed, score counter separated into funtion

--- bowling.py
+++ bowling.py
@@ -1,42 +1,30 @@
 def score(game):
+    if game[-3] == 'X' or game[-3] == 'x':
+        return how_to_count(game, len(game) - 2)
+    else:
+        return how_to_count(game, len(game))
+
+
+def how_to_count(game, length):
     result = 0
-    frame = 1
-    in_first_half = True
-    for i in range(len(game)):
+    for i in range(length):
         if game[i] == '/':
-            result += 10 - last
+                result += 10 - get_value(game[i-1]) + get_value(game[i+1])
+        elif game[i] == 'X' or game[i] == 'x':
+            result += get_value(game[i]) + get_value(game[i+1])
+            if game[i+2] == '/':
+                result += 10 - get_value(game[i+1])
+            else:
+                result += get_value(game[i+2])
         else:
             result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
-        if frame < 10 and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
-                else:
-                    result += get_value(game[i+2])
-        last = get_value(game[i])
-        if not in_first_half:
-            frame += 1
-        if in_first_half is True:
-            in_first_half = False
-        else:
-            in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
-            in_first_half = True
-            frame += 1
     return result
 
 
-def get_value(char):
+def get_value(char):  # Values for rolls in game
     if char in str([i for i in range(1, 10)]):
         return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
+    elif char == 'X' or char == 'x' or char == '/':
         return 10
     elif char == '-':
         return 0
