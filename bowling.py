07fdfc1ce3fa6a93d4af3cef4169b46b9a47aab5
Adrienn Dánics <adrienn.d91@gmail.com>
2017-11-26 16:17:44 +0100

    Comments added, lower added

--- bowling.py
+++ bowling.py
@@ -1,30 +1,41 @@
 def score(game):
-    if game[-3] == 'X' or game[-3] == 'x':
-        return how_to_count(game, len(game) - 2)
+    '''Get overall score of game from string.
+    Args:
+        game: rolls
+    Returns: result/score'''
+    if game[-3].lower() == 'x':
+        return how_to_count(game, len(game) - 2)  # discounting extra rolls that are strikes
     else:
         return how_to_count(game, len(game))
 
 
 def how_to_count(game, length):
+    '''Get the values of the rolls in the game and adds them into overall score.
+    Args:
+        game: rolls
+        length: the number of rolls to evaluate)
+    Returns: score'''
     result = 0
     for i in range(length):
-        if game[i] == '/':
-                result += 10 - get_value(game[i-1]) + get_value(game[i+1])
-        elif game[i] == 'X' or game[i] == 'x':
-            result += get_value(game[i]) + get_value(game[i+1])
+        if game[i] == '/':  # get a (10+next score) but discount previous roll
+            result += get_value(game[i]) + get_value(game[i+1]) - get_value(game[i-1])
+        elif game[i].lower() == 'x':  # get (10+next two rolls)
+            result += get_value(game[i]) + get_value(game[i+1]) + get_value(game[i+2])
             if game[i+2] == '/':
-                result += 10 - get_value(game[i+1])
-            else:
-                result += get_value(game[i+2])
+                result -= get_value(game[i+1])
         else:
             result += get_value(game[i])
     return result
 
 
-def get_value(char):  # Values for rolls in game
+def get_value(char):
+    '''Get the values of individual rolls/characters in string
+    Args:
+        char: char to be examined in game string
+    Returns: int value'''
     if char in str([i for i in range(1, 10)]):
         return int(char)
-    elif char == 'X' or char == 'x' or char == '/':
+    elif char.lower() == 'x' or char == '/':
         return 10
     elif char == '-':
         return 0
